e111d8b01022d317c187bfe4aa550093ed8a3bd9
DwieTwarze2000 <48262070+DwieTwarze2000@users.noreply.github.com>
2021-12-01 18:48:06 +0100

    power calculator js

---
+++ javascript/veryEasy/powerCalculator.js
@@ -0,0 +1,7 @@
+// Create a function that takes voltage and current and returns the calculated power.
+// circuitPower(230, 10) ➞ 2300
+// circuitPower(110, 3) ➞ 330
+
+const circuitPower = (voltage, current) => {
+    return voltage * current;
+};
