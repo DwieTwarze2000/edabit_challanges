7eaf1a3291eef4d9f48a1a3367a6a6149d785a3d
DwieTwarze2000 <48262070+DwieTwarze2000@users.noreply.github.com>
2021-12-10 01:36:13 +0100

    Simple OOP Calculator js

---
+++ javascript/veryEasy/simpleOOPCalculator.js
@@ -0,0 +1,19 @@
+// Create functions for the Calculator class that can do the following:
+// Add two numbers.
+// Subtract two numbers.
+// Multiply two numbers.
+// Divide two numbers.
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+}
